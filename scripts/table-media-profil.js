e5c7109902d74c1be1069fcc467ec0620645ae09
Santiago Lee <lihe@mini1.cn>
2023-12-22 18:23:35 +0800

    Add profil rewrite

---
+++ scripts/table-media-profil.js
@@ -0,0 +1,119 @@
+$done({
+    body: JSON.stringify({
+        "message": "OK.",
+        "userId": 3443,
+        "email": "howdy@gmail.com",
+        "sfUserData": {
+            "SalesforceUserId": "003S7000002fRuSIAU",
+            "userAttributes": {
+                "firstname": "Maximilian",
+                "lastname": "Heinrich",
+                "salutation": "Mr.",
+                "academic_title": null,
+                "company": "Kein Unternehmen (Website)"
+            },
+            "licenses": [
+                {},
+                {
+                    "table": "china_DE",
+                    "license": "Full",
+                    "startDate": "2023-12-22T07:36:45.000+0000",
+                    "expireDate": null,
+                    "id": "a5QS70000002YiLMAU"
+                },
+                {
+                    "table": "africa_DE",
+                    "license": "Full",
+                    "startDate": "2023-12-22T07:36:45.000+0000",
+                    "expireDate": null,
+                    "id": "a5QS70000002YiMMAU"
+                },
+                {
+                    "table": "bildung_DE",
+                    "license": "Full",
+                    "startDate": "2023-12-22T07:36:45.000+0000",
+                    "expireDate": null,
+                    "id": "a5QS70000002YiNMAU"
+                },
+                {
+                    "table": "climate_DE",
+                    "license": "Full",
+                    "startDate": "2023-12-22T07:36:45.000+0000",
+                    "expireDate": null,
+                    "id": "a5QS70000002YiOMAU"
+                },
+                {
+                    "table": "agrifood_DE",
+                    "license": "Full",
+                    "startDate": "2023-12-22T07:36:45.000+0000",
+                    "expireDate": null,
+                    "id": "a5QS70000002YiPMAU"
+                },
+                {
+                    "table": "esg_DE",
+                    "license": "Full",
+                    "startDate": "2023-12-22T07:36:45.000+0000",
+                    "expireDate": null,
+                    "id": "a5QS70000002YiQMAU"
+                },
+                {
+                    "table": "europe_DE",
+                    "license": "Full",
+                    "startDate": "2023-12-22T07:36:45.000+0000",
+                    "expireDate": null,
+                    "id": "a5QS70000002YiRMAU"
+                },
+                {
+                    "table": "security_DE",
+                    "license": "Full",
+                    "startDate": "2023-12-22T07:36:45.000+0000",
+                    "expireDate": null,
+                    "id": "a5QS70000002YiSMAU"
+                },
+                {
+                    "table": "research_DE",
+                    "license": "Full",
+                    "startDate": "2023-12-22T07:36:45.000+0000",
+                    "expireDate": null,
+                    "id": "a5QS70000002YiTMAU"
+                },
+                {
+                    "table": "100headlines_DE",
+                    "license": "Free",
+                    "startDate": "2023-12-22T07:36:46.000+0000",
+                    "expireDate": null,
+                    "id": "a5QS70000002YiUMAU"
+                },
+                {
+                    "table": "berlin_DE",
+                    "license": "Free",
+                    "startDate": "2023-12-22T07:36:46.000+0000",
+                    "expireDate": null,
+                    "id": "a5QS70000002YiVMAU"
+                },
+                {
+                    "table": "china_EN",
+                    "license": "Full",
+                    "startDate": "2023-12-22T10:05:07.000+0000",
+                    "expireDate": null,
+                    "id": "a5QS70000002Z1hMAE"
+                },
+                {
+                    "table": "europe_EN",
+                    "license": "Full",
+                    "startDate": "2023-12-22T10:05:07.000+0000",
+                    "expireDate": null,
+                    "id": "a5QS70000002Z1iMAE"
+                },
+                {
+                    "table": "climate_EN",
+                    "license": "Full",
+                    "startDate": "2023-12-22T10:05:07.000+0000",
+                    "expireDate": null,
+                    "id": "a5QS70000002Z1jMAE"
+                }
+            ],
+            "status": 200
+        }
+    })
+});
