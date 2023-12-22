6e7875c0cc352d2dd0e03c4b92556cf0901f12e0
Santiago Lee <lihe@mini1.cn>
2023-12-22 19:14:55 +0800

    Change license type

--- scripts/table-media-profil.js
+++ scripts/table-media-profil.js
@@ -16,98 +16,98 @@ $done({
                 {},
                 {
                     "table": "china_DE",
-                    "license": "Free",
+                    "license": "Paid",
                     "startDate": "2023-12-22T07:36:45.000+0000",
                     "expireDate": null,
                     "id": "a5QS70000002YiLMAU"
                 },
                 {
                     "table": "africa_DE",
-                    "license": "Free",
+                    "license": "Paid",
                     "startDate": "2023-12-22T07:36:45.000+0000",
                     "expireDate": null,
                     "id": "a5QS70000002YiMMAU"
                 },
                 {
                     "table": "bildung_DE",
-                    "license": "Free",
+                    "license": "Paid",
                     "startDate": "2023-12-22T07:36:45.000+0000",
                     "expireDate": null,
                     "id": "a5QS70000002YiNMAU"
                 },
                 {
                     "table": "climate_DE",
-                    "license": "Free",
+                    "license": "Paid",
                     "startDate": "2023-12-22T07:36:45.000+0000",
                     "expireDate": null,
                     "id": "a5QS70000002YiOMAU"
                 },
                 {
                     "table": "agrifood_DE",
-                    "license": "Free",
+                    "license": "Paid",
                     "startDate": "2023-12-22T07:36:45.000+0000",
                     "expireDate": null,
                     "id": "a5QS70000002YiPMAU"
                 },
                 {
                     "table": "esg_DE",
-                    "license": "Free",
+                    "license": "Paid",
                     "startDate": "2023-12-22T07:36:45.000+0000",
                     "expireDate": null,
                     "id": "a5QS70000002YiQMAU"
                 },
                 {
                     "table": "europe_DE",
-                    "license": "Free",
+                    "license": "Paid",
                     "startDate": "2023-12-22T07:36:45.000+0000",
                     "expireDate": null,
                     "id": "a5QS70000002YiRMAU"
                 },
                 {
                     "table": "security_DE",
-                    "license": "Free",
+                    "license": "Paid",
                     "startDate": "2023-12-22T07:36:45.000+0000",
                     "expireDate": null,
                     "id": "a5QS70000002YiSMAU"
                 },
                 {
                     "table": "research_DE",
-                    "license": "Free",
+                    "license": "Paid",
                     "startDate": "2023-12-22T07:36:45.000+0000",
                     "expireDate": null,
                     "id": "a5QS70000002YiTMAU"
                 },
                 {
                     "table": "100headlines_DE",
-                    "license": "Free",
+                    "license": "Paid",
                     "startDate": "2023-12-22T07:36:46.000+0000",
                     "expireDate": null,
                     "id": "a5QS70000002YiUMAU"
                 },
                 {
                     "table": "berlin_DE",
-                    "license": "Free",
+                    "license": "Paid",
                     "startDate": "2023-12-22T07:36:46.000+0000",
                     "expireDate": null,
                     "id": "a5QS70000002YiVMAU"
                 },
                 {
                     "table": "china_EN",
-                    "license": "Free",
+                    "license": "Paid",
                     "startDate": "2023-12-22T10:05:07.000+0000",
                     "expireDate": null,
                     "id": "a5QS70000002Z1hMAE"
                 },
                 {
                     "table": "europe_EN",
-                    "license": "Free",
+                    "license": "Paid",
                     "startDate": "2023-12-22T10:05:07.000+0000",
                     "expireDate": null,
                     "id": "a5QS70000002Z1iMAE"
                 },
                 {
                     "table": "climate_EN",
-                    "license": "Free",
+                    "license": "Paid",
                     "startDate": "2023-12-22T10:05:07.000+0000",
                     "expireDate": null,
                     "id": "a5QS70000002Z1jMAE"
