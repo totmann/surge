9f73f4207791fd1861a15cbea2eb6dd595ff1081
Santiago Lee <lihe@mini1.cn>
2023-12-25 18:15:11 +0800

    Debug

--- scripts/republik.js
+++ scripts/republik.js
@@ -2,8 +2,8 @@ let obj = JSON.parse($response.body);
 if (obj.data.me.username) {
     obj.data.me.activeMembership = true;
 }
-else if (obj.data.meGuidance) {
-    obj.data.meGuidance.activeMembership = true;
-}
+// else if (obj.data.meGuidance) {
+//     obj.data.meGuidance.activeMembership = true;
+// }
 
 $done({body: JSON.stringify(obj)});
