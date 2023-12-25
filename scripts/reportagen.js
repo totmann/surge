f82332162ee5687300837545e2405427fa053764
Santiago Lee <lihe@mini1.cn>
2023-12-25 18:38:25 +0800

    Bug fix

--- scripts/reportagen.js
+++ scripts/reportagen.js
@@ -1,9 +1,11 @@
-let obj = JSON.parse($response.body);
-if (obj.data.login) {
-    obj.data.login.user.subscriptionAccessStatus = true;
+if ($response.body) {
+    let obj = JSON.parse($response.body);
+    if (obj.data.login) {
+        obj.data.login.user.subscriptionAccessStatus = true;
+    }
+    else if (obj.data.viewer) {
+        obj.data.viewer.subscriptionAccessStatus = true;
+    }
+    
+    $done({body: JSON.stringify(obj)});
 }
-else if (obj.data.viewer) {
-    obj.data.viewer.subscriptionAccessStatus = true;
-}
-
-$done({body: JSON.stringify(obj)});
--- scripts/republik.js
+++ scripts/republik.js
@@ -1,9 +1,11 @@
-let obj = JSON.parse($response.body);
-if (obj.data.me.username) {
-    obj.data.me.activeMembership = true;
+if ($response.body) {
+    let obj = JSON.parse($response.body);
+    if (obj.data.me.username) {
+        obj.data.me.activeMembership = true;
+    }
+    else if (obj.data.meGuidance) {
+        obj.data.meGuidance.activeMembership = true;
+    }
+    
+    $done({body: JSON.stringify(obj)});
 }
-// else if (obj.data.meGuidance) {
-//     obj.data.meGuidance.activeMembership = true;
-// }
-
-$done({body: JSON.stringify(obj)});
