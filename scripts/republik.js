e273843bcbfa92d69e3250c80b4420885ea376f2
Santiago Lee <lihe@mini1.cn>
2023-12-25 18:05:39 +0800

    Bug fix

--- scripts/republik.js
+++ scripts/republik.js
@@ -1,6 +1,9 @@
 let obj = JSON.parse($response.body);
-if (obj.data.me) {
+if (obj.data.me.username) {
     obj.data.me.activeMembership = true;
+}
+else if (obj.data.meGuidance) {
+    obj.data.meGuidance.activeMembership = true;
 }
 
 $done({body: JSON.stringify(obj)});
