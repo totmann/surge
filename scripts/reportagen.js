45400769a41b1c5f78866c20718d370a6c6aa287
Santiago Lee <lihe@mini1.cn>
2023-12-25 11:06:12 +0800

    Debug

--- scripts/reportagen.js
+++ scripts/reportagen.js
@@ -1,4 +1,4 @@
-let body = JSON.parse($response.body);
-console.log(body + "\n" + $response.body);
+// let mbody = JSON.parse($response.body);
+console.log($response.body);
 // $body['data']['login']['user']['subscriptionAccessStatus'] = true;
 // $done({body: JSON.stringify(body)});
