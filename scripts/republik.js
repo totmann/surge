2cacd41e01af833aa0334485e08eaab075b1ed21
Santiago Lee <cheshiry@gmail.com>
2023-12-25 22:18:38 +0800

    Remove role

--- scripts/republik.js
+++ scripts/republik.js
@@ -2,7 +2,6 @@ if ($response.body) {
     let obj = JSON.parse($response.body);
     if (obj.data && obj.data.me && obj.data.me.username) {
         obj.data.me.activeMembership = true;
-        obj.data.me.roles = ["member"];
     }
     else if (obj.data && obj.data.meGuidance) {
         obj.data.meGuidance.activeMembership = true;
