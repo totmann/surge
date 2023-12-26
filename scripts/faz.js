aca4d4ebf26b6717f425e736154fa27c8713eee3
Santiago Lee <cheshiry@gmail.com>
2023-12-27 02:24:31 +0800

    Bug fix

--- scripts/faz.js
+++ scripts/faz.js
@@ -1,6 +1,6 @@
 if ($response.body) {
     let obj = JSON.parse($response.body);
-    obj[0].abos = ["FAS-EPAP"]
+    obj[0].pdfPlaceIds = [2553,1167,1171,2557,1995,1221,2551,2559]
     
     $done({body: JSON.stringify(obj)});
 }
