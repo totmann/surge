328b6b4eff8edfe3d2b16ecc644d1523719dcbce
Santiago Lee <cheshiry@gmail.com>
2023-12-27 01:00:15 +0800

    Bug fix

--- scripts/faz.js
+++ scripts/faz.js
@@ -1,6 +1,6 @@
 if ($response.body) {
     let obj = JSON.parse($response.body);
-    obj[0].abos = ["FAS-EPAP", "FAZ-EPAP", "FAQ-DIGITAL"]
+    obj[0].abos = ["FAS-EPAP", "FAZ-ARCHIV", "FAQ-DIGITAL"]
     
     $done({body: JSON.stringify(obj)});
 }
