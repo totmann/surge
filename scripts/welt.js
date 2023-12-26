694aecb942482a4ecb07200459e774a80b8c683c
Santiago Lee <cheshiry@gmail.com>
2023-12-26 22:40:30 +0800

    Bug fix

--- scripts/welt.js
+++ scripts/welt.js
@@ -1,9 +1,8 @@
 $done({
-    body: JSON.stringify([
-        {
-            "promotionId": "O_J5NJA42Y08G43L2O9G",
-            "from": "19-11-2022 20:15:30",
-            "to": "19-11-2042 20:15:30"
-        }
-    ])
+    body: JSON.stringify({
+        "auth_id": "ASAG8726266",
+        "is_premium": true,
+        "display_name": "santiago l.",
+        "is_moderator": true
+    })
 });
