ab6cbf474e763badaea5ca0f4f03181e00253a81
Santiago Lee <lihe@mini1.cn>
2023-12-26 17:37:30 +0800

    Add Welt

---
+++ scripts/welt.js
@@ -0,0 +1,9 @@
+$done({
+    body: JSON.stringify([
+        {
+            "promotionId": "O_J5NJA42Y08G43L2O9G",
+            "from": "19-11-2022 20:15:30",
+            "to": "19-11-2042 20:15:30"
+        }
+    ])
+});
