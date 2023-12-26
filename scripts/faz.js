986a557ec92d362b801873da464fc4b6dd76b0e9
Santiago Lee <cheshiry@gmail.com>
2023-12-27 00:40:33 +0800

    Add faz

---
+++ scripts/faz.js
@@ -0,0 +1,8 @@
+if ($response.body) {
+    let obj = JSON.parse($response.body);
+    obs[0].abos = ["plenigoAbo", "purAccess", "digitecAccess", "einspruchAccess", "fplusMultiuserAbo", "werbefrei", "FAS-EPAP", "FAZ-ARCHIV", "FAZ-EPAP", "FAQ-DIGITAL"]
+    
+    $done({body: JSON.stringify(obj)});
+}
+
+$done({});
--- scripts/welt.js
+++
@@ -1,8 +0,0 @@
-$done({
-    body: JSON.stringify({
-        "auth_id": "ASAG8726266",
-        "is_premium": true,
-        "display_name": "santiago l.",
-        "is_moderator": true
-    })
-});
