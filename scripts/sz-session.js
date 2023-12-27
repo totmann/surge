f04f119ce76b0436f894e0ce2fd114ad51198437
Santiago Lee <lihe@mini1.cn>
2023-12-27 19:01:50 +0800

    Bug fix

--- scripts/sz-session.js
+++ scripts/sz-session.js
@@ -2,7 +2,8 @@ if ($response.body) {
     let obj = JSON.parse($response.body);
     obj.subscriptionInformation = {
         "state": "ACTIVE",
-        "end": "2999-12-31"
+        "end": "2999-12-31",
+        "subscriptionType": "SZ_DIGITAL_PACKAGE_L"
     }
     
     $done({body: JSON.stringify(obj)});
