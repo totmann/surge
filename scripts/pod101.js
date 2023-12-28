3d80e00e92b9f350e558340feefc98089f6cc2e0
Santiago Lee <cheshiry@gmail.com>
2023-12-28 21:26:08 +0800

    Bug fix

--- scripts/pod101.js
+++ scripts/pod101.js
@@ -1,10 +1,8 @@
 if ($response.body) {
     let obj = JSON.parse($response.body);
-    obj.data.Subscription.bnrtxt = "";
-    obj.data.Subscription.btntxt = "";
-    obj.data.Subscription.message = "";
     obj.data.Subscription.hasPremiumPlusAccess = true;
     obj.data.Subscription.subscriptionType = "premium+";
+    obj.data.Subscription.subscription = "premium+";
     obj.data.Subscription.renewdate = "2047-08-07T00:00:00+09:00";
     obj.data.Subscription.status = "active";
     obj.data.Subscription.pplus_begin_date = "2022-08-07T00:00:00+09:00";
