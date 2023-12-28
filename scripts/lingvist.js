d8819cbb2314e1a38040dc596be216216c3c8017
Santiago Lee <lihe@mini1.cn>
2023-12-28 19:24:03 +0800

    Bug fix

--- scripts/lingvist.js
+++ scripts/lingvist.js
@@ -1,6 +1,6 @@
 if ($response.body) {
     let obj = JSON.parse($response.body);
-    obj.subscription.status = "unlimited";
+    obj.subscription.status = "paid";
     obj.subscription.expiration_ts = "2047-06-10T07:04:20Z";
     
     $done({body: JSON.stringify(obj)});
