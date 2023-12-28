27a00860f56b3762a945e2f2cfcd0d8411204a66
Santiago Lee <cheshiry@gmail.com>
2023-12-28 23:45:56 +0800

    Bug fix

--- scripts/pod101.js
+++ scripts/pod101.js
@@ -1,15 +1,28 @@
 if ($response.body) {
     let obj = JSON.parse($response.body);
-    if (obj.data && obj.data.Subscription) {
-        obj.data.Subscription.bnrtxt = "";
-        obj.data.Subscription.btntxt = "";
-        obj.data.Subscription.message = "";
-        obj.data.Subscription.hasPremiumAccess = true;
-        obj.data.Subscription.subscriptionType = "premium";
-        obj.data.Subscription.subscription = "premium";
-        obj.data.Subscription.renewdate = "2047-08-07T00:00:00+09:00";
-        obj.data.Subscription.status = "active";
-        obj.data.Subscription.pplus_begin_date = "2022-08-07T00:00:00+09:00";
+    if (obj.data) {
+        if (obj.data.Subscription) {
+            obj.data.Subscription.bnrtxt = "";
+            obj.data.Subscription.btntxt = "";
+            obj.data.Subscription.message = "";
+            obj.data.Subscription.hasPremiumAccess = true;
+            obj.data.Subscription.subscriptionType = "premium";
+            obj.data.Subscription.subscription = "premium";
+            obj.data.Subscription.renewdate = "2047-08-07T00:00:00+09:00";
+            obj.data.Subscription.status = "active";
+            obj.data.Subscription.pplus_begin_date = "2022-08-07T00:00:00+09:00";
+        } 
+        else if (obj.data.bnrtxt) {
+            obj.data.bnrtxt = "";
+            obj.data.btntxt = "";
+            obj.data.message = "";
+            obj.data.hasPremiumAccess = true;
+            obj.data.subscriptionType = "premium";
+            obj.data.subscription = "premium";
+            obj.data.renewdate = "2047-08-07T00:00:00+09:00";
+            obj.data.status = "active";
+            obj.data.pplus_begin_date = "2022-08-07T00:00:00+09:00";
+        }
     }
     
     $done({body: JSON.stringify(obj)});
