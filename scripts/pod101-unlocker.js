d2f64f59f54fcd56c12607639e463d15d3526319
Santiago Lee <cheshiry@gmail.com>
2023-12-28 23:34:20 +0800

    Bug fix

--- scripts/pod101-unlocker.js
+++ scripts/pod101-unlocker.js
@@ -1,15 +1,17 @@
 if ($response.body) {
     let obj = JSON.parse($response.body);
     if (obj.data)
-        obj.data.Subscription.bnrtxt = "";
-        obj.data.Subscription.btntxt = "";
-        obj.data.Subscription.message = "";
-        obj.data.Subscription.hasPremiumAccess = true;
-        obj.data.Subscription.subscriptionType = "premium";
-        obj.data.Subscription.subscription = "premium";
-        obj.data.Subscription.renewdate = "2047-08-07T00:00:00+09:00";
-        obj.data.Subscription.status = "active";
-        obj.data.Subscription.pplus_begin_date = "2022-08-07T00:00:00+09:00";
+        if (obj.data.Subscription) {
+            obj.data.Subscription.bnrtxt = "";
+            obj.data.Subscription.btntxt = "";
+            obj.data.Subscription.message = "";
+            obj.data.Subscription.hasPremiumAccess = true;
+            obj.data.Subscription.subscriptionType = "premium";
+            obj.data.Subscription.subscription = "premium";
+            obj.data.Subscription.renewdate = "2047-08-07T00:00:00+09:00";
+            obj.data.Subscription.status = "active";
+            obj.data.Subscription.pplus_begin_date = "2022-08-07T00:00:00+09:00";
+        }
         if (obj.data.Path && obj.data.Path.Entities) {
             for (i in obj.data.Path.Entities) {
                 obj.data.Path.Entities[i].Locked = false;
