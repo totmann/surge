5a26df8256e95f98e4ef05dcc00161fb90ec5957
Santiago Lee <cheshiry@gmail.com>
2024-01-08 21:31:09 +0800

    Bug fix

--- scripts/ti.js
+++ scripts/ti.js
@@ -1,7 +1,8 @@
 if ($response.body) {
     let obj = JSON.parse($response.body);
     if (obj.user) {
-        obj.user.access_level = 2;
+        obj.user.access_level = 1;
+        obj.user.is_pro_subscriber = true;
         obj.user.can_actually_comment = true;
         obj.user.company_name = "ByteDance";
         obj.user.is_corp_or_group_subscriber = true;
