eede285e8477030abd32814a10f79b540d27c90a
Santiago Lee <lihe@mini1.cn>
2024-01-09 18:44:34 +0800

    Bug fix

--- scripts/courrier.js
+++ scripts/courrier.js
@@ -1,7 +1,7 @@
 if ($response.body) {
     let obj = JSON.parse($response.body);
     if (obj.data) {
-        obj.data.useService.courrier.useStatus = 9;
+        obj.data.useService.courrier.useStatus = 12;
         obj.data.useService.courrier.endDate = "2044-01-09T08:19:22.000Z";
     }
     
