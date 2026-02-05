6fbf7129cd36c011135e8f653dbc782d6c9a265f
Santiago Lee <cheshiry@gmail.com>
2026-02-05 22:36:04 +0800

    bug fix

--- scripts/zanichelli.js
+++ scripts/zanichelli.js
@@ -1,8 +1,6 @@
 if ($response.body) {
     let obj = JSON.parse($response.body);
-    if (obj.id) {
-        obj.is_employee = 1;
-    }
+    obj.is_employee = 1;
     
     $done({body: JSON.stringify(obj)});
 }
